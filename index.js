22aba854d829d7a819c92e1c0e302531cb38efd0
Bima Junestifa <bimajunestifa17@gmail.com>
2026-05-22 21:54:41 +0700

    comit1

--- index.js
+++ index.js
@@ -2,22 +2,13 @@ import jsonfile from "jsonfile";
 import moment from "moment";
 import simpleGit from "simple-git";
 
-const file = "data.json";
-const date = moment().format();
+const path = "data.json";
+const date = moment().subtract(5, 'days').format();
+
 const data = {
-    date,
+  date: date,
 };
 
-try {
-    await jsonfile.writeFile(file, data, { spaces: 2 });
-
-    const git = simpleGit();
-    await git.add([file]);
-    await git.commit(date, { "--date": date });
-    await git.push();
-
-    console.log(`Committed ${file} with date ${date}`);
-} catch (error) {
-    console.error("CLI failed:", error.message);
-    process.exit(1);
-}
+jsonfile.writeFile(path, data, () => {
+  simpleGit().add([path]).commit(date, { '--date': date }).push();
+});
