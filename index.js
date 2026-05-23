8bc86b27500ed803a4a31f7e8b34a89d3c4f12a5
Bima Junestifa <bimajunestifa17@gmail.com>
2026-05-23 11:35:54 +0700

    comit2

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ import moment from "moment";
 import simpleGit from "simple-git";
 
 const path = "data.json";
-const date = moment().subtract(5, 'days').format();
+const git = simpleGit();
 
-const data = {
-  date: date,
+const daysToFill = Number(process.env.DAYS_TO_FILL ?? 120);
+const commitsPerDay = Number(process.env.COMMITS_PER_DAY ?? 3);
+
+const makeCommit = async (date) => {
+  const data = { date };
+
+  await jsonfile.writeFile(path, data, { spaces: 2 });
+  await git.add([path]);
+  await git.commit(date, { "--date": date });
+  console.log(`Committed ${date}`);
 };
 
-jsonfile.writeFile(path, data, () => {
-  simpleGit().add([path]).commit(date, { '--date': date }).push();
-});
+for (let dayOffset = daysToFill - 1; dayOffset >= 0; dayOffset -= 1) {
+  for (let commitIndex = 0; commitIndex < commitsPerDay; commitIndex += 1) {
+    const date = moment()
+      .subtract(dayOffset, "days")
+      .hour(12)
+      .minute(commitIndex * 10)
+      .second(0)
+      .format();
+
+    await makeCommit(date);
+  }
+}
+
+await git.push();
+console.log(`Pushed ${daysToFill * commitsPerDay} commits to origin.`);
